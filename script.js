e13907cd69010adf16710a9c991bea2539bed353
parvesh74 <131499170+parvesh74@users.noreply.github.com>
2023-04-21 18:14:34 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+src="https://unpkg.com/typed.js@2.0.15/dist/typed.umd.js"
+
+var typed = new Typed(document.getElementById("autotype"),{
+    strings:["I'm Parvesh","I'm a Student at BVC"],
+    typeSpeed: 150,
+    backSpeed: 200,
+    loop: true
+});
